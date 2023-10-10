4edce32f860f3ad3d9216a57840949e49ec91995
Qifu <qianfu@live.nl>
2023-10-10 22:33:00 +0200

    S6

--- opdracht/S6_Views_student.sql
+++ opdracht/S6_Views_student.sql
@@ -12,18 +12,43 @@
 --
 -- 1. Maak een view met de naam "deelnemers" waarmee je de volgende gegevens uit de tabellen inschrijvingen en uitvoering combineert:
 --    inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum, uitvoeringen.docent, uitvoeringen.locatie
--- 2. Gebruik de view in een query waarbij je de "deelnemers" view combineert met de "personeels" view (behandeld in de les):
---     CREATE OR REPLACE VIEW personeel AS
--- 	     SELECT mnr, voorl, naam as medewerker, afd, functie
---       FROM medewerkers;
--- 3. Is de view "deelnemers" updatable ? Waarom ?
+CREATE OR REPLACE VIEW deelnemers AS
+SELECT i.cursist, i.cursus, i.begindatum, u.docent, u.locatie
+FROM
+    inschrijvingen i
+JOIN
+    uitvoeringen u ON i.cursus = u.cursus AND i.begindatum = u.begindatum;
 
+-- 2. Gebruik de view in een query waarbij je de "deelnemers" view combineert met de "personeels" view (behandeld in de les):
+CREATE OR REPLACE VIEW personeel AS
+SELECT mnr, voorl, naam as medewerker, afd, functie
+FROM medewerkers;
+SELECT deelnemers.cursist, personeel.medewerker, deelnemers.cursus, deelnemers.begindatum, deelnemers.docent, deelnemers.locatie
+FROM deelnemers
+JOIN personeel ON personeel.mnr = deelnemers.cursist;
+-- 3. Is de view "deelnemers" updatable ? Waarom ?
+-- het is updatable, omdat het nog eenvoudig is
 
 -- S6.2.
 --
 -- 1. Maak een view met de naam "dagcursussen". Deze view dient de gegevens op te halen: 
 --      code, omschrijving en type uit de tabel curssussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt. 
+CREATE OR REPLACE VIEW dagcursussen AS
+SELECT code, omschrijving, type
+FROM cursussen
+WHERE LENGTH(code) = 1;
+
+SELECT * FROM dagcursussen;
+
 -- 2. Maak een tweede view met de naam "daguitvoeringen". 
 --    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
--- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+CREATE OR REPLACE VIEW daguitvoeringen AS
+SELECT u.cursus, u.begindatum, u.docent, u.locatie
+FROM uitvoeringen u
+JOIN dagcursussen d ON u.cursus = d.code;
 
+SELECT * FROM daguitvoeringen;
+
+-- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+DROP view dagcursussen CASCADE;
+DROP view daguitvoeringen RESTRICT;
